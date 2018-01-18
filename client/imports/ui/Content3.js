6d4d6c1c32f3239c1c005881e4de7034d94c563d
junekim <migkjy@naver.com>
2018-01-18 07:11:28 -0800

    bug fixed

--- client/imports/ui/Content3.js
+++ client/imports/ui/Content3.js
@@ -15,7 +15,7 @@ const ContentSecond = () => (
         <div className="col-lg-3 col-md-6 text-center">
           <div className="service-box mt-5 mx-auto">
             <div className="card">
-              <img className="card-img-top" src="/images/cardpic1.jpg" alt="Card image cap" />
+              <img className="card-img-top" src="/images/cardpic1.png" alt="Card image cap" />
               <div className="card-body">
                 <h3>신축</h3>
                 {/* <h3 className="mb-3">신축</h3> */}
@@ -27,7 +27,7 @@ const ContentSecond = () => (
         <div className="col-lg-3 col-md-6 text-center">
           <div className="service-box mt-5 mx-auto">
             <div className="card">
-              <img className="card-img-top" src="/images/cardpic2.jpg" alt="Card image cap" />
+              <img className="card-img-top" src="/images/cardpic2.png" alt="Card image cap" />
               <div className="card-body">
                 <h3 className="mb-3">리모델링</h3>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -38,7 +38,7 @@ const ContentSecond = () => (
         <div className="col-lg-3 col-md-6 text-center">
           <div className="service-box mt-5 mx-auto">
             <div className="card">
-              <img className="card-img-top" src="/images/cardpic3.jpg" alt="Card image cap" />
+              <img className="card-img-top" src="/images/cardpic3.png" alt="Card image cap" />
               <div className="card-body">
                 <h3 className="mb-3">인테리어</h3>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -49,7 +49,7 @@ const ContentSecond = () => (
         <div className="col-lg-3 col-md-6 text-center">
           <div className="service-box mt-5 mx-auto">
             <div className="card">
-              <img className="card-img-top" src="/images/cardpic4.jpg" alt="Card image cap" />
+              <img className="card-img-top" src="/images/cardpic4.png" alt="Card image cap" />
               <div className="card-body">
                 <h3 className="mb-3">수리</h3>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>

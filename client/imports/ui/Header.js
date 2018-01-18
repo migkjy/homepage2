594d4371700fe866b6cb8174e7adcaef6bec6b6c
junekim <migkjy@naver.com>
2018-01-18 06:49:27 -0800

    deleted some unused files

--- client/imports/ui/Header.js
+++ client/imports/ui/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Header = () => (
-  <header id="home" className="py-5 bg-image-full" style={{ backgroundImage: 'url("https://unsplash.it/1900/1080?image=1076")' }}>
-    <div className="container" />
-    <img className="img-fluid d-block mx-auto" src="http://placehold.it/200x200&text=Logo" />
+  <header id="home" className="py-5 bg-image-full" style={{ backgroundImage: 'url("/images/pic1.jpg")' }}>
+    <div className="container" style={{ height: 200 }} >
+      <img className="img-fluid d-block mx-auto" src="/images/logo0.png" />
+    </div>
   </header>
 );
 
--- client/imports/ui/Image.js
+++ client/imports/ui/Image.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Image = () => (
-  <section className="py-5 bg-image-full" style={{ backgroundImage: 'url("https://unsplash.it/1900/1080?image=1081")' }}>
+  <section className="py-5 bg-image-full" style={{ backgroundImage: 'url("/images/pic2.jpg")' }}>
     {/* Put anything you want here! There is just a spacer below for demo purposes! */}
     <div style={{ height: 200 }} />
   </section>
